9ce091234f5b4ef9c51c802933d4d2bb5db3d153
Akhil Thakur <takhil498@gmail.com>
2025-11-18 23:00:57 +0530

    vite-config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
+// vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Add this line to force relative paths for assets
+  base: './', 
   plugins: [react()],
-  base: '/TRAVELO/'  // Add this line - should match your repo name
 })
